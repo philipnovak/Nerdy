cd83f14b5bb4d1af75562b6f77ad47f25ee91add
Philip Novak <philipfnovak@gmail.com>
2017-02-28 11:46:11 -0500

    Added jQuery UI and some graphics

--- nerdy.js
+++ nerdy.js
@@ -1,38 +1,41 @@
-var operator = "mult";
+var operator = "*";
+var locked;
+var unlocked;
 
 // TODO: consolidate operators, make variables lockable
 function valuechange(element) {
 	var var1 = document.getElementById("var1");
 	var var2 = document.getElementById("var2");
 	var product = document.getElementById("product");
-	if(operator == "mult") {
+	if(operator == "*") {
 		switch(element.id) {
 			case "var1": product.value = var1.value * var2.value; break;
 			case "var2": product.value = var1.value * var2.value; break;
 			case "product": var2.value = product.value / var1.value; break;
 		}
 	}
-	else if(operator == "div") {
-		switch(element.id) {
-			case "var1": product.value = var1.value / var2.value; break;
-			case "var2": product.value = var1.value / var2.value; break;
-			case "product": var1.value = product.value * var2.value; break;
-		}
-	}
-	else if(operator == "add") {
+	else if(operator == "+") {
 		switch(element.id) {
 			case "var1": product.value = +var1.value + +var2.value; break;
 			case "var2": product.value = +var1.value + +var2.value; break;
 			case "product": var2.value = product.value - var1.value; break;
 		}
 	}
-	else if(operator == "sub") {
+	else if(operator == "/") {
+		switch(element.id) {
+			case "var1": product.value = var1.value / var2.value; break;
+			case "var2": product.value = var1.value / var2.value; break;
+			case "product": var1.value = product.value * var2.value; break;
+		}
+	}
+	else if(operator == "-") {
 		switch(element.id) {
 			case "var1": product.value = var1.value - var2.value; break;
 			case "var2": product.value = var1.value - var2.value; break;
 			case "product": var2.value = var1.value - product.value; break;
 		}
 	}
+	console.log(var1.value + " " + operator + " " + var2.value);
 }
 
 function operatorchange(element) {
@@ -40,12 +43,40 @@ function operatorchange(element) {
 	valuechange(document.getElementById("var2"));
 }
 
+function keyinput(event, element) {
+	if(/[*+/-]/.test(event.key)) {
+		event.preventDefault();
+		var operatorelement = document.getElementById("operator");
+		operatorelement.value = event.key;
+		operatorchange(operatorelement);
+		document.getElementById("var2").select();
+	}
+}
+
 function xscroll(event, element) {
-	if(event.wheelDelta >= 120) {
-		element.value++;
+	element.blur();
+	if(event.wheelDelta >= 1) {
+		if(event.shiftKey) element.value = +element.value + +10;
+		else element.value++;
 	}
 	else {
-		element.value--;
+		if(event.shiftKey) element.value -= 10;
+		else element.value--;
 	}
 	valuechange(element);
 }
+
+function lockvar(element, lockid) {
+	if(element.parentElement.className.indexOf("unlocked") > -1) {
+		element.parentElement.className = "inputwrapper locked";
+		locked = lockid;
+	}
+	else {
+		element.parentElement.className = "inputwrapper unlocked";
+		unlocked = lockid;
+	}
+}
+
+$(function() {
+	$(".inputwrapper").draggable();
+});

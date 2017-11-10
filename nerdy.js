71982c086e00f0a27317731d542f474ce2d13447
Philip Novak <philipfnovak@gmail.com>
2017-11-10 11:58:43 -0500

    Reimagine & refactor
    - Bring this project back to life
    - Incorporate algebra.js from nicolewhite
    - Remove jQuery UI & graphics

--- nerdy.js
+++ nerdy.js
@@ -1,91 +1,164 @@
-"use strict";
+'use strict';
 
-var inputs;
+/* Initialize Algebra.js */
+var Fraction = algebra.Fraction;
+var Expression = algebra.Expression;
+var Equation = algebra.Equation;
+
+var expression,
+	equation,
+	inputs,
+	selected;
 
 var mathTable = {
-	"*": {
-		"n1": function() { return  n3.value / n2.value  },
-		"n2": function() { return  n3.value / n1.value  },
-		"n3": function() { return  n1.value * n2.value  }
+	'*': {
+		'n1': function() { return  n3.value / n2.value  },
+		'n2': function() { return  n3.value / n1.value  },
+		'n3': function() { return  n1.value * n2.value  }
 	},
-	"+": {
-		"n1": function() { return  n3.value - n2.value  },
-		"n2": function() { return  n3.value - n1.value  },
-		"n3": function() { return +n1.value + +n2.value }
+	'+': {
+		'n1': function() { return  n3.value - n2.value  },
+		'n2': function() { return  n3.value - n1.value  },
+		'n3': function() { return +n1.value + +n2.value }
 	},
-	"/": {
-		"n1": function() { return  n3.value * n2.value  },
-		"n2": function() { return  n1.value / n3.value  },
-		"n3": function() { return  n1.value / n2.value  }
+	'/': {
+		'n1': function() { return  n3.value * n2.value  },
+		'n2': function() { return  n1.value / n3.value  },
+		'n3': function() { return  n1.value / n2.value  }
 	},
-	"-": {
-		"n1": function() { return +n3.value + +n2.value },
-		"n2": function() { return  n1.value - n3.value  },
-		"n3": function() { return  n1.value - n2.value  }
+	'-': {
+		'n1': function() { return +n3.value + +n2.value },
+		'n2': function() { return  n1.value - n3.value  },
+		'n3': function() { return  n1.value - n2.value  }
+	}
+}
+
+var operatorTable = {
+	'*': function(n) { return expression.multiply(n) },
+	'+': function(n) { return expression.add(n) },
+	'/': function(n) { return expression.divide(n) },
+	'-': function(n) { return expression.subtract(n) }
+}
+
+function evaluateEquation() {
+	expression = new Expression('n1');
+	expression = operatorTable[o1.value]('n2');
+
+	equation = new Equation(expression, +n3.value);
+
+	console.log(equation.toString());
+}
+
+function solveForSelected() {
+	if (selected !== n3) {               //    .---- This will need to be refactored
+		var evaluation = {};             //    |    to only iterate through expression1
+		inputs.forEach(function(input) { //    v
+			if (input !== selected && input !== n3) {
+				evaluation[input.id] = +input.value;
+			}
+		});
+		console.log(evaluation);
+
+		equation = new Equation(expression.eval(evaluation), +n3.value);
+
+		console.log(equation.toString());
+		console.log(selected.id + ' = ' + equation.solveFor(selected.id).toString());
+	}
+	else {
+		var evaluation = {};
+		inputs.forEach(function(input) {
+			if (input !== selected) {
+				evaluation[input.id] = +input.value;
+			}
+		});
+		console.log(evaluation);
+
+		console.log(selected.id + ' = ' + expression.eval(evaluation));
 	}
 }
 
 function valueChange() {
-	for (var i = 0; i < inputs.length; i++) {
-		var input = inputs[i];
-		if (this === input || input.isLocked()) {
-			continue;
+	if (selected === this) {
+		if (this !== n3) {
+			select(n3);
+			this.valueChange();
 		}
 		else {
-			input.value = mathTable[o1.value][input.id]();
-			break;
+			select(n1);
+			this.valueChange();
 		}
+	}
+	else {
+		selected.value = mathTable[o1.value][selected.id]();
 	}
 }
 
-function keyInput() {
-	if (/[*+/-]/.test(event.key)) {
-		event.preventDefault();
-		o1.value = event.key;
-		n2.select();
-	}
+function operatorChange() {
+	expression = new Expression('n1');
+	expression = operatorTable[o1.value]('n2');
+	expression2 = new Expression('n3');
+
+	equation = new Equation(expression, expression2);
+
+	console.log(equation.toString());
+
+	n2.valueChange();
 }
 
 function wheelSpin() {
 	event.preventDefault();
 	this.blur();
 	if (event.wheelDelta >= 1) {
-		if (event.ctrlKey && event.shiftKey) this.value = +this.value + +1000;
-		else if (event.ctrlKey) this.value = +this.value + +100;
-		else if (event.shiftKey) this.value = +this.value + +10;
-		else if (event.altKey) this.value *= 2;
-		else this.value++;
+		this.value = 
+			event.ctrlKey &&
+			event.shiftKey ? +this.value + 1000 :
+			event.ctrlKey  ? +this.value + 100  :
+			event.shiftKey ? +this.value + 10   :
+			event.altKey   ?  this.value * 2    :
+			                 +this.value + 1    ;
 	}
 	else {
-		if (event.ctrlKey && event.shiftKey) this.value -= 1000;
-		else if (event.ctrlKey) this.value -= 100;
-		else if (event.shiftKey) this.value -= 10;
-		else if (event.altKey) this.value /= 2;
-		else this.value--;
+		this.value = 
+			event.ctrlKey &&
+			event.shiftKey ? this.value - 1000 :
+			event.ctrlKey  ? this.value - 100  :
+			event.shiftKey ? this.value - 10   :
+			event.altKey   ? this.value / 2    :
+			                 this.value - 1    ;
 	}
 	this.valueChange();
 }
 
-function lockNum() {
-	event.preventDefault();
-	this.classList.toggle("locked");
+function select(input) {
+	if (!input.isSelected()) {
+		selected.classList.remove('selected');
+		selected = input;
+		selected.classList.add('selected');
+	}
 }
 
-function isLocked() {
-	return this.classList.contains("locked");
+function isSelected(id) {
+	if (this instanceof HTMLInputElement) {
+		return this === selected;
+	}
+	if (id) {
+		return id === selected.id;
+	}
+	console.warn("Invalid argument or context supplied for function isSelected");
 }
 
 window.onload = function() {
-	inputs = [n3, n1, n2];
+	inputs = document.querySelectorAll('input');
 
 	inputs.forEach(function(input) {
 		input.valueChange = valueChange;
-		input.onkeypress = keyInput;
 		input.oninput = valueChange;
 		input.onwheel = wheelSpin;
-		input.oncontextmenu = lockNum;
-		input.isLocked = isLocked;
+		input.isSelected = isSelected;
+		input.onclick = function() { select(this) };
 	});
 
-	o1.onchange = valueChange;
+	selected = document.querySelector('.selected');
+
+	o1.onchange = n2.valueChange;
 }
